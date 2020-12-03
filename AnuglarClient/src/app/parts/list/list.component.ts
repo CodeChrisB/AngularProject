de7b88e35384aa1dad0806dbeed304ae5c7cf091
CodeChrisB <c.buchberger.01@gmail.com>
2020-12-03 22:58:41 +0100

    Table design

--- AnuglarClient/src/app/parts/list/list.component.ts
+++ AnuglarClient/src/app/parts/list/list.component.ts
@@ -4,6 +4,23 @@ const customers = [
 
 ]
 
+interface todo {
+  person:string,
+  descripton:string,
+  priority:number,
+  urgency:string,
+  length:number
+}
+
+const todo: todo[] = [
+  {"person":"Chris","descripton":"descripton","priority":1,"urgency":"urgency","length":1,},
+  {"person":"Chris","descripton":"descripton","priority":2,"urgency":"urgency","length":1},
+  {"person":"Chris","descripton":"descripton","priority":3,"urgency":"urgency","length":1},
+  {"person":"Chris","descripton":"descripton","priority":3,"urgency":"urgency","length":1},
+  {"person":"Chris","descripton":"descripton","priority":2,"urgency":"urgency","length":1},
+  {"person":"Chris","descripton":"descripton","priority":2,"urgency":"urgency","length":1},
+  {"person":"Chris","descripton":"descripton","priority":4,"urgency":"urgency","length":1}
+]
 
 
 @Component({
@@ -13,23 +30,42 @@ styleUrls: ['./list.component.css']
 })
 export class List  {
   //init the data
-  displayedColumns = [""];
+  displayedColumns = ["Person","Description","Priority","Urgency", "Length","Actions"];
 
 
    // MatPaginator Inputs
-   length = customers.length;
+   length = todo.length;
    pageSize = 10;
    pageSizeOptions: number[] = [5, 10];
-   dataSource = customers.slice(0,this.pageSize);
+   dataSource = todo.slice(0,this.pageSize);
    goToPage($event){
-
      this.length = $event.length;
      this.pageSize =$event.pageSize;
-     this.dataSource = customers.slice(this.pageSize* $event.pageIndex,this.pageSize* $event.pageIndex+this.pageSize)
+     this.dataSource = this.dataSource.slice(this.pageSize* $event.pageIndex,this.pageSize* $event.pageIndex+this.pageSize)
    }
 
   onRowClicked(row) {
     console.log('Row clicked: ', row);
+  }
+
+  getColor(id:number) :string{
+    if(id==1)
+      return 'red';
+    if(id==2)
+      return '#FF8800';
+    if(id==3)
+      return 'green';
+    return 'grey'
+  }
+
+  getPrior(id:number){
+    if(id==1)
+    return 'Major';
+  if(id==2)
+    return 'Medium';
+  if(id==3)
+    return 'Minor';
+  return 'None'
   }
 
 
